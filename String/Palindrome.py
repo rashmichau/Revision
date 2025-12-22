75f8cfdf1249593d5f9cdc3d0cd91d66e17a1ebc
rashmichau <161075049+rashmichau@users.noreply.github.com>
2025-12-21 23:19:06 -0600

    add corner cases

--- String/Palindrome.py
+++ String/Palindrome.py
@@ -1,8 +1,11 @@
 # Check if string is symmetrical or palindrome
 
 s = input("enter any string : ")
+s=s.strip()
 mid=len(s)//2
-if s[:mid]==s[mid:]:
+if(len(s)==0):
+    print("enter a valid string!!!!!")
+elif s[:mid]==s[mid:]:
     print("string is symmetric")
 elif s==s[ : :-1]:
     print("string is palindrome")
---
+++ String/reverse_str.py
@@ -0,0 +1,6 @@
+# Reverse words in a String
+s= input("enter string : ")
+print(s[ : :-1])
+
+# another method
+
