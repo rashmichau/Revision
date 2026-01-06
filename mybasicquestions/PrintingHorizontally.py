d427d67bd60c8d79d2b24a612d88a0afc3265188
rashmichau <161075049+rashmichau@users.noreply.github.com>
2026-01-05 19:17:01 -0600

    daily practice question

---
+++ mybasicquestions/PrintingHorizontally.py
@@ -0,0 +1,8 @@
+# input :[1,3,6,7]
+# output : 1 3 6 7
+# print horizontally group of items
+l = [3,5,7,8,9]
+for i in l:
+    print(i,end=" ")
+print()
+print("1","5","2026",sep="/")
---
+++ mystring/UncommonWordsFromTwoString.py
@@ -0,0 +1,7 @@
+# Find uncommon words from two Strings
+# Input : A = "Geeks for Geeks"
+#         B = "Learning from Geeks for Geeks"
+# Output: ['Learning', 'from'].
+a = input("Enter any string sentence : ").split()
+b = input("Enter any sentence : ").split()
+print(a and b)
