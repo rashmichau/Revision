f00556fb068489e738a1c64b9737fecfec2c44f6
rashmichau <161075049+rashmichau@users.noreply.github.com>
2026-01-08 19:09:18 -0600

    swapping  variables

---
+++ mybasicquestions/SwapingVariables.py
@@ -0,0 +1,21 @@
+# Swaping Variables
+# input:x=10
+#       y=15
+# output:x=15
+#        y=10
+x = input("Enter anything : ")
+y = input("enter anything : ")
+
+# method 1
+# x,y=y,x
+# print("x=",x)
+# print("y=",y)
+
+
+# method 2
+tran=x
+x=y
+y=tran
+print("x=",x)
+print("y=",y)
+
